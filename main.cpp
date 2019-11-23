da189c03d348101905e760543284a926a7633d76
C.O.Marius <marrius9876@gmail.com>
2019-11-23 14:12:06 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include "main.h"
-
+#include <fcntl.h>    /* For O_RDWR */
+#include <unistd.h>
 const FUNCS_* _ptr;
 
 int main(int argc, char *argv[])
 {
     _ptr = load();
 
-    int o = PF_v4l2_ioctl(55,55);
-
+    int o = PF_v4l2_open("/dev/video0", O_RDWR | O_NONBLOCK);
+    if(o)
+    {
+        PF_v4l2_close(o);
+    }
 
 
 }
